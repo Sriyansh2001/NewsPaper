6dcf6e70dd86fd56cf8751a3802c137f737ee5bb
Sriyansh srivastava <sriyansh2001@gmail.com>
2023-05-20 15:07:28 +0530

    Finsh of Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React ,{useState} from "react";
 import './navbar.css';
 
 const Navbar = () => {
+    const [mobile_menu,change_mobile_menu] = useState(false);
+
+    const change_state_of_menu = () => {
+        change_mobile_menu(!mobile_menu);
+    }
+
     return (
     <>
         <nav className="main-nav">
             <div className="logo">
                 <h3>Digital NewsPaper</h3>
             </div>
-            <div className="menu-link">
+            <div className={mobile_menu?"menu-link mobile-menu-link":"menu-link"}>
                 <ul type="none">
                     <li><a href="/">Home</a></li>
                     <li><a href="/">About</a></li>
@@ -22,6 +28,9 @@ const Navbar = () => {
                     <li><a href="/">YT</a></li>
                     <li><a href="/">In</a></li>
                 </ul>
+            </div>
+            <div className="hamburger">
+                <button onClick={change_state_of_menu}>Menu</button>
             </div>
         </nav>
         <div className="main_section">
