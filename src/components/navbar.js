c81fb20f20ef288f07ab6594f5808a1a7ecf79aa
Sriyansh srivastava <sriyansh2001@gmail.com>
2023-06-14 00:42:19 +0530

    Updating navbar for mobile view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,40 @@
-import React ,{Component} from "react";
+import React ,{Component}  from "react";
 import './navbar.css';
-
 
 
 class Navbar extends Component {
     constructor() {
         super();
         this.state = {
-            mobile_menu:false
+            innerWidth:window.innerWidth,
+            work:false,
+            height:"0px",
+            width: "0%",
+            right:"200px"
         };
         this.change_state_of_menu = this.change_state_of_menu.bind(this);
     }
 
     change_state_of_menu() {
-        this.setState({
-            mobile_menu:(!this.state.mobile_menu)
+        this.setState ({
+            innerWidth:window.innerWidth
         })
+        if(!this.state.work) {
+            this.setState({
+                height:"300px",
+                width:"100%",
+                right:"0px",
+                work:true
+            })
+        }
+        else {
+            this.setState({
+                height:"0px",
+                width:"0%",
+                right:"200px",
+                work:false
+            })
+        }
     }
 
     render() {
@@ -26,17 +45,19 @@ class Navbar extends Component {
                 <div className="logo">
                     <h3>Digital NewsPaper</h3>
                 </div>
-                <div className={this.state.mobile_menu?"mobile-menu-link menu-link":"menu-link"}>
-                    <ul type="none">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/">Business</a></li>
-                        <li><a href="/">Entertainment</a></li>
-                        <li><a href="/">General</a></li>
-                        <li><a href="/">Health</a></li>
-                        <li><a href="/">Science</a></li>
-                        <li><a href="/">Sports</a></li>
-                        <li><a href="/">Technology</a></li>
-                    </ul>
+                <div style={this.state.innerWidth<=1020?{height:this.state.height}:{}} className="menu-link" >
+                    <div style={this.state.innerWidth<=1020?{right:this.state.right , width:this.state.width}:{}} className="links" >
+                        <ul type="none">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">Business</a></li>
+                            <li><a href="/">Entertainment</a></li>
+                            <li><a href="/">General</a></li>
+                            <li><a href="/">Health</a></li>
+                            <li><a href="/">Science</a></li>
+                            <li><a href="/">Sports</a></li>
+                            <li><a href="/">Technology</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="social-media">
                     <ul className="social-media-desktop" type="none">
